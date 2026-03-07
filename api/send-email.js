f2f7f2d63012d2fbb1ddea65f0bdb1a0e249934e
sabih75 <sabihulh75@gmail.com>
2026-03-08 01:35:30 +0500

    Add API route for contact form email

--- api/send-email.js
+++ api/send-email.js
@@ -1,11 +1,11 @@
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+//   if (req.method !== "POST") {
+//     return res.status(405).json({ message: "Method not allowed" });
+//   }
 
-  const { name, email, subject, message } = req.body;
+//   const { name, email, subject, message } = req.body;
 
   try {
     const transporter = nodemailer.createTransport({
